2a513048e6efacb5614f03bf6f8f5a837905f873
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2021-12-03 12:42:26 -0300

    feat: Adicionado o uso de códigos de barras usados como identificadores das boletas
    Está presente no código a geração de um código de barras que atua como nº de série das boletas e que é inserido em sua assinatura digital, eliminando o uso de mais um valor de 2048 bits escolhido em Fp.

--- digitalSignatureEG.py
+++ digitalSignatureEG.py
@@ -1,8 +1,9 @@
+from barcode.writer import ImageWriter
 from Cryptodome.Random import random
-from Cryptodome.Hash import SHA256
 from Cryptodome.Util import number
 from pyzbar.pyzbar import decode
 from datetime import datetime
+from barcode import EAN13
 import cv2 as cv
 import qrcode
 import os
@@ -11,12 +12,19 @@ import os
 now = datetime.now()
 out_qr_name = 'QRCode_DS_EG_' + (str(now.date()) + '_' + str(now.hour) + '_' +
                                    str(now.minute) + '_' + str(now.second) + '.jpg')
-out_img_name = 'Boleta_Assinada_' + (str(now.date()) + '_' + str(now.hour) + '_' +
+out_bar_name = 'Codigo_Barras_' + (str(now.date()) + '_' + str(now.hour) + '_' +
+                                   str(now.minute) + '_' + str(now.second))
+out_img_name1 = 'Boleta_Identificada_' + (str(now.date()) + '_' + str(now.hour) + '_' +
+                                   str(now.minute) + '_' + str(now.second) + '.jpg')
+out_img_name2 = 'Boleta_Assinada_' + (str(now.date()) + '_' + str(now.hour) + '_' +
                                    str(now.minute) + '_' + str(now.second) + '.jpg')
 boletas = '/Novas Boletas/'
-saida = '/Assinaturas/'
-if not os.path.isdir(os.getcwd() + saida):
-    os.mkdir(os.getcwd() + saida)
+codes = '/Assinaturas/'
+bar = '/Barras/'
+if not os.path.isdir(os.getcwd() + codes):
+    os.mkdir(os.getcwd() + codes)
+if not os.path.isdir(os.getcwd() + bar):
+    os.mkdir(os.getcwd() + bar)
 
 
 def codifica_msg(p):
@@ -59,7 +67,7 @@ def keys_generator(p, q, g):
     return private_key, public_key
 
 
-def sign_elgamal(cod, p, q, private_key):
+def sign_elgamal(cod, g, p, q, private_key):
 
     # Geração da chave efêmera e sua inversa
     ephemeral_key = random.randint(1, q - 1)
@@ -85,17 +93,21 @@ def verify_elgamal(s1, s2, public_key, p, g, cod):
 
 def restore_sign_pair(signature):
 
-    sign_size = len(signature)
+    check = 0
+    for i in range(len(signature)):
 
-    for i in range(sign_size):
-        if signature[i] == ',':
-            div = i
+        if signature[i] == ',' and not check:
+            div1 = i
+            check = 1
+        elif signature[i] == ',' and check:
+            div2 = i
             break
 
-    p1 = signature[1 : div]
-    p2 = signature[div + 2 : sign_size - 1]
+    cod = signature[1 : div1]
+    p1 = signature[div1 + 2 : div2]
+    p2 = signature[div2 + 2 : len(signature) - 1]
 
-    return int(p1), int(p2)
+    return int(cod), int(p1), int(p2)
 
 
 def encrypt_elgamal(g, q, p, public_key, cod):
@@ -116,26 +128,56 @@ def decrypt_elgamal(c1, c2, private_key, p):
     return msg
 
 
+def place_barcode(img, codigo_barra):
+
+    # Redimensiona o código de barras
+    res = cv.resize(codigo_barra, dsize = (523, 80), interpolation = cv.INTER_CUBIC)
+    height, width = res.shape[:2]
+
+    # Insere o código de barras na boleta
+    img[80 : 80 + height, 50 : 50 + width] = res
+
+    # Salva as alterações
+    cv.imwrite(os.getcwd() + boletas + out_img_name1, img)
+
+    return img
+
+
+def read_barcode(img):
+
+    # Destaca o código de barras
+    codigo_barra = img[70 : 150, 80 : 550]
+    info = decode(codigo_barra)
+
+    # Obtém o número de série
+    for i in info:
+        n_serie = i.data.decode('utf-8')
+
+    return int(n_serie)
+
+
 def place_QRCode(img, qrc):
 
-    # Redimensiona a imagem
+    # Redimensiona o QR Code
     res = cv.resize(qrc, dsize = (120, 160), interpolation = cv.INTER_CUBIC)
     height, width = res.shape[:2]
 
-    # Cola o QRCode na boleta
-    img[10 : 10 + height, 425 : 425 + width] = res
+    # Insere o QRCode na boleta
+    img[175 : 175 + height, 760 : 760 + width] = res
 
     # Salva as alterações
-    cv.imwrite(os.getcwd() + boletas + out_img_name, img)
+    cv.imwrite(os.getcwd() + boletas + out_img_name2, img)
 
     return img
 
 
 def read_QRCode(img):
   
-    qr_code = img[5 : 160, 410 : 545]
+    # Destaca o QR Code da boleta
+    qr_code = img[175 : 540, 760 : 880]
     info = decode(qr_code)
 
+    # Obtém o par da assinatura
     for i in info:
         signature = i.data.decode("utf-8")
     
@@ -145,39 +187,47 @@ def read_QRCode(img):
 if __name__ == '__main__':
 
     # Gera os números primos e a base g de ordem q em Fp
-    n = 256                       # Mínimo ---> 2048 bits
+    n = 256                      # Mínimo ---> 2048 bits
     p, q = primes_generator(n)
     g = base_g(p, q)
 
     # Obtém a chave secreta e a chave pública
     private_key, public_key = keys_generator(p, q, g)
 
-    # Codifica a boleta como um número pertencente a Fp escolhido de forma aleatória
-    boleta = str(codifica_msg(p))
+    # Criação do código de barras de 12 dígitos
+    min = pow(10, 11)
+    max = pow(10, 12) - 1
+    numero = random.randint(min, max)
+    c_bar = EAN13(str(numero), writer = ImageWriter())
+    c_bar.save(os.getcwd() + bar + out_bar_name)
 
-    # Obtém o hash criptográfico da codificação da boleta
-    resumo = SHA256.new(bytes(boleta, 'utf-8'))
-    boleta_codificada = int(resumo.hexdigest(), 16)
+    # Obtém a boleta e seu código de barras identificador correspondente
+    boleta = cv.imread(os.getcwd() + boletas + 'teste1.jpg')
+    codigo_barra = cv.imread(os.getcwd() + bar + out_bar_name + '.png')
 
-    # Gera a assinatura digital El Gamal da boleta e seu QR Code correspondente
-    s1, s2 = sign_elgamal(boleta_codificada, p, q, private_key)
-    signed = (s1, s2)
-    print('\nPar da assinatura: ', signed)
+    # Insere o código de barras na boleta e realiza sua leitura para obter o nº de série
+    boleta_id = place_barcode(boleta, codigo_barra)
+    n_serie = read_barcode(boleta_id)
+
+    # Gera a assinatura digital El Gamal da boleta
+    s1, s2 = sign_elgamal(n_serie, g, p, q, private_key)
+    signed = (n_serie, s1, s2)
+    print('\nAssinatura: ', signed)
 
     # Gera o QR Code da assinatura digital realizada
     qrc = qrcode.make(signed)
-    qrc.save(os.getcwd() + saida + out_qr_name)
+    qrc.save(os.getcwd() + codes + out_qr_name)
 
-    # Obtém a boleta e seu QR Code contendo a assinatura correspondente
-    qr_code = cv.imread(os.getcwd() + saida + out_qr_name)
-    boleta = cv.imread(os.getcwd() + boletas + 'teste4.jpg')
+    # Obtém a boleta identificada com o nº de série e seu QR Code contendo a assinatura correspondente
+    boleta_barcode = cv.imread(os.getcwd() + boletas + out_img_name1)
+    qr_code = cv.imread(os.getcwd() + codes + out_qr_name)
     
     # Insere o QR Code na boleta e realiza sua leitura para obter a assinatura
-    boleta_assinada = place_QRCode(boleta, qr_code)
+    boleta_assinada = place_QRCode(boleta_barcode, qr_code)
     signature = read_QRCode(boleta_assinada)
-    print('\nPar da assinatura lida do QR Code: ', signature)
+    print('\nAssinatura lida do QR Code: ', signature)
     
     # Executa o algoritmo de verificação para a assinatura lida do QR Code
-    p1, p2 = restore_sign_pair(signature)
-    verify = verify_elgamal(p1, p2, public_key, p, g, boleta_codificada)
+    cod, p1, p2 = restore_sign_pair(signature)
+    verify = verify_elgamal(p1, p2, public_key, p, g, cod)
     print('\nStatus da verificação: ', verify)
