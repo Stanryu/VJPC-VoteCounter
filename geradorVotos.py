4a26ab2c5b63ef9de7ee51cfb2d778992a04e6fb
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-09-27 07:23:43 -0300

    feat: Adicionado métodos para obter o layout de boleta
    Agora o layout da boleta é obtido de acordo com o nome da eleição informado. Além disso, este é passado como parâmetro junto à lista de dígitos para cada cargo e as escolhas de voto informadas para marcação na boleta em branco base obtida.

--- geradorVotos.py
+++ geradorVotos.py
@@ -1,31 +1,54 @@
-import os
 from electionConfiguration import readConfigFile
+from voteMarking import marking
+import cv2 as cv
+import os
 
-caminho = os.getcwd() + '/Static/Cargas/'
+
+caminho = os.getcwd() + '/Static'
+loads = '/Cargas/'
+logos = '/Logotipos/'
 
 
 def getConfigFile(nome):
-    for _, _, arquivo in os.walk(caminho):
+    for _, _, arquivo in os.walk(caminho + loads):
         for file in arquivo:
             if nome in file:
-                cargo, posi, digitos = readConfigFile(file)
+                cargo, posi, digitos, _ = readConfigFile(file)
                 return cargo, digitos, posi
 
 
-if __name__ == '__main__':
-    name = str(input("Digite o nome da eleição:"))
-    cargos, digito, pos = getConfigFile(name)
+def get_ballot_layout(name):
+
+    for _, _, arquivo in os.walk(caminho + logos):
+        for file in arquivo:
+            if name in file:
+                ballot_layout = cv.imread(caminho + logos + file)
+                return ballot_layout
+
+
+def main():
+
+    name = str(input("Digite o nome da eleição: "))
+    cargos, digito, _ = getConfigFile(name)
+    ballot_layout = get_ballot_layout(name)
     cont = 0
-    votos = {}
+    votos = list()
+
     for elem in cargos:
         flag = 0
+        
         while flag == 0:
-            voto = input(f"Qual seu voto para o cargo {elem} ({digito[cont]} digitos): ")
+            voto = input(f"Qual seu voto para o cargo {elem} ({digito[cont]} dígitos): ")
+            
             if len(voto) == int(digito[cont]):
-                votos[elem] = voto
+                votos.append(str(voto))
                 cont += 1
                 flag = 1
             else:
-                print("Número de digitos invalido. Vote novamente")
+                print("Número de digitos inválido. Vote novamente!")
 
-    print(votos)
+    marking(ballot_layout, digito, tuple(votos))
+
+
+if __name__ == '__main__':
+    main()
