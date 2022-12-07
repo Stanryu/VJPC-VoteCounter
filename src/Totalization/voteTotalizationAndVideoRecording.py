8bda9fbe0a3ba1cd7a6910efd65747ec85cfb13c
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-12-07 03:20:53 -0300

    refact: Flexibilizada a totalização para quaisquer tipos de eleições
    Os scripts agora são capazes de totalizar boletas presentes no diretório de urna de uma eleição, conforme o arquivo de configuração contendo os candidatos, para então exibir o resultado. O vídeo continua sendo gerado da mesma forma, embora a totalização pelo mesmo também tenha sido modificada. De forma geral, é possível aplicar o processo de totalização por diretório e vídeo, além da geração do mesmo para quaisquer configurações de eleições.

--- src/Totalization/voteTotalizationAndVideoRecording.py
+++ src/Totalization/voteTotalizationAndVideoRecording.py
@@ -1,80 +1,137 @@
-# -*- coding: utf-8 -*-
-from datetime import datetime
 import cv2 as cv
+import json
 import sys
 import os
 
 sys.path.append(os.getcwd() + '/src/')
-import electionConfiguration
-import readVote
-import imprimeResultado
-from electionConfiguration import general_data
+from electionConfiguration import getConfigFile
+from readVote import run2 as read_ballot
+from imprimeResultado import run as print_result
+from directories import *
 
-# Define dados de seção eleitoral
-zona = '256'
-secao = "1"
-nFrames = 30
-now = datetime.now()
-out_file_name = 'Totalizacao--Zona_' + zona + '-Secao_' + secao + '-Data' + (str(now.date()) + '_' + str(now.hour)+'-' +
-                                                                             str(now.minute)+'-'+str(now.second) + '.avi')
 
-# Formato do vídeo (Avi)
-fourcc = cv.VideoWriter_fourcc('M', 'J', 'P', 'G')
+def generate_video():
 
-# Diretórios
-ver = '/v1.0'
-urna = '/UrnaFisica/'
-saida = '/Output/'
+    try:
+        election_name = str(input("Informe o nome da eleição: "))
+    except ValueError:
+        print('Insira informações válidas!')
 
-# Os diretótios 'election_data', '/Output/' e '/v1.0/ são criados caso não existam
-if not os.path.isdir(os.getcwd() + general_data):
-    os.mkdir(os.getcwd() + general_data)
-if not os.path.isdir(os.getcwd() + general_data + ver):
-    os.mkdir(os.getcwd() + general_data + ver)
-if not os.path.isdir(os.getcwd() + general_data + ver + saida):
-    os.mkdir(os.getcwd() + general_data + ver + saida)
+    # Formato do vídeo (Avi)
+    fourcc = cv.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    nFrames = 30
 
-# Lista de nomes dos arquivos (boletas)
-boletas = os.listdir(os.getcwd() + general_data + ver + urna)
+    # Diretório da urna
+    urna = '/' + election_name + '_Urn/'
+    if not os.path.isdir(current_dir + general_data + ver + urna):
+        raise NotADirectoryError('O diretório não existe! Cheque o nome da eleição e se existem registros para gerar o vídeo.')
 
-# Identifica tamanhos das imagens
-img = cv.imread(os.getcwd() + general_data + ver + urna + boletas[0])
-height, width, layers = img.shape
+    # Lista de nomes dos arquivos (boletas)
+    boletas = os.listdir(current_dir + general_data + ver + urna)
 
-# Cria escritor de vídeo com base nas configurações identificadas
-out = cv.VideoWriter(os.getcwd() + general_data + ver + saida + out_file_name, fourcc, nFrames, (width, height))
+    # Identifica tamanhos das imagens
+    img = cv.imread(os.getcwd() + general_data + ver + urna + boletas[0])
+    height, width, _ = img.shape
 
-# Executa a configuração da eleição
-config_file, qtd_cargos = electionConfiguration.configElection()
-cargos, linhas, campos = electionConfiguration.readConfigFile(config_file, qtd_cargos)
+    # Cria escritor de vídeo com base nas configurações identificadas
+    out = cv.VideoWriter(os.getcwd() + general_data + ver + videos + election_name + '_TallyVideo.avi', fourcc, nFrames, (width, height))
 
-# Inicializa mapas de votação
-votos = list()
-for i in range(qtd_cargos):
-    votos.append({})
+    # Identifica cada voto e contabiliza no mapa de votação
+    for boleta in boletas:
 
-# Identifica cada voto e contabiliza no mapa de votação
-for boleta in boletas:
+        # Adiciona a imagem da boleta em um frame do vídeo
+        img = cv.imread(os.getcwd() + general_data + ver + urna + boleta)
+        out.write(img)
 
-    # Adiciona a imagem da boleta em um frame do vídeo
-    img = cv.imread(os.getcwd() + general_data + ver + urna + boleta)
-    out.write(img)
+    # Fecha janelas e finaliza escritor
+    cv.destroyAllWindows()
+    out.release()
 
-    # Lê os votos da boleta atual
-    tupla_votos = readVote.run2(img, campos)
 
-    # Contabiliza os votos da boleta
+def tallying():
+
+    try:
+        election_name = str(input("Informe o nome da eleição: "))
+    except ValueError:
+        print('Insira informações válidas!')
+
+    # Diretório da urna
+    urna = '/' + election_name + '_Urn/'
+    if not os.path.isdir(current_dir + general_data + ver + urna):
+        raise NotADirectoryError('O diretório não existe! Cheque o nome da eleição e se existem registros para totalizar.')
+
+    # Lista de nomes dos arquivos (boletas)
+    boletas = os.listdir(current_dir + general_data + ver + urna)
+
+    # Obtém o arquivo de configuração e os dados necessários para iniciar o processo de totalização
+    cargos, _, _, campos = getConfigFile(election_name)
+    if not cargos:
+        raise FileNotFoundError('Não existe arquivo de configuração para a eleição informada!')
+    qtd_cargos = len(cargos)
+
+    # Inicializa mapas de votação
+    votos = list()
     for i in range(qtd_cargos):
-        key = tupla_votos[i]
-        votos[i][key] = votos[i][key] + \
-            1 if key in votos[i] else 1
+        votos.append(dict())
 
-# Fecha janelas e finaliza escritor
-cv.destroyAllWindows()
-out.release()
+    # Identifica cada voto e contabiliza no mapa de votação
+    for boleta in boletas:
 
-# Imprime resultado
-for i in range(qtd_cargos):
-    imprimeResultado.run(votos[i], cargos[i])
+        # Adiciona a imagem da boleta em um frame do vídeo
+        img = cv.imread(os.getcwd() + general_data + ver + urna + boleta)
 
-# TODO: Adicionar validação da contagem pelo vídeo com o número de boletas na urna física.
+        # Lê os votos da boleta atual
+        tupla_votos = read_ballot(img, campos)
+        
+        # Contabiliza os votos da boleta
+        for i in range(qtd_cargos):
+            key = tupla_votos[i]
+            votos[i][key] = votos[i][key] + \
+                1 if key in votos[i] else 1
+    
+    # Obtém o nome do arquivo de configuração
+    check = False
+    for _, _, files in os.walk(os.getcwd() + general_data + stat + output):
+        for file in files:
+            
+            if election_name in file:
+                file_election = file
+                check = True
+                break
+        
+        if check:
+            break
+    
+    # Monta o dicionário de candidatos {número de voto : nome do candidato}
+    candidates = dict()
+    with open(os.getcwd() + general_data + stat + output + file_election, 'r') as file:
+
+        data = json.load(file)
+
+        for cargo in data['Cargos']:
+            for value in cargo['Candidatos']:
+                candidates[value] = cargo['Candidatos'][value]
+
+    candidates[''] = 'Votos em Branco'
+
+    # Imprime resultado
+    for i in range(qtd_cargos):
+        print_result(votos[i], cargos[i], candidates)
+
+    # TODO: Adicionar validação da contagem pelo vídeo com o número de boletas na urna física.
+
+
+if __name__ == '__main__':
+
+    print('1 - Gerar Vídeo\n2 - Totalizar\n')
+    try:
+        option = int(input("Informe a operação: "))
+    except ValueError:
+        print('Insira valores inteiros!')
+    
+    if option == 1:
+        generate_video()
+    elif option == 2:
+        tallying()
+    else:
+        print('Opção Inválida!')
