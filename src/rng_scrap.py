4a887031fba6e84a54fc822457441034c31ba3ef
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-02-11 19:18:55 -0300

    refact: Agora o navegador só é encerrado após gerar todos os códigos
    Anteriormente, a cada "lote" de números o navegador era encerrado, fazendo assim com que fosse reaberto e requisitada a página Web novamente, caso ainda não tenham sido gerada a quantidade de números desejada.

--- src/rng_scrap.py
+++ src/rng_scrap.py
@@ -1,69 +1,94 @@
+from random_user_agent.params import OperatingSystem
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName
 from Cryptodome.Random import random
 from bs4 import BeautifulSoup
+from mechanize import Browser
 from time import sleep
-import mechanize
-import requests
 
 
-n = 1000
-# total = 10000
-total = 1
+# Total de números para gerar, intervalo dos mesmos e página Web para gerar
+total = 10000000
 min = pow(10, 11)
 max = pow(10, 12) - 1
 rng_url = 'https://www.gigacalculator.com/calculators/random-number-generator.php'
 
-
-def input_rng():
-
-    browser = mechanize.Browser()
-
-    browser.set_handle_robots(False)
-    browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/109.0')]
-    browser.open(rng_url)
-
-    browser.select_form(nr = 1)
-    browser.form['numbersfrom'] = str(min)
-    browser.form['numbersto'] = str(max)
-    browser.form['number'] = str(n)
-
-    browser.submit()
-    response = browser.response().read()
-    browser.close()
-
-    return response
+# Geração das strings de agente de usuário (para vários SOs e navegadores distintos)
+software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.EDGE.value, SoftwareName.OPERA.value, SoftwareName.SAFARI.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.ANDROID.value, OperatingSystem.IOS.value, OperatingSystem.MACOS.value]   
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=500)
 
 
-def rng_scrap(raw_html):
+def initialize_browser():
+
+    my_browser = Browser()
+    my_browser.set_handle_robots(False)
+
+    # Requisita como um User-agent distinto a cada requisição para evitar bloqueios por IP
+    my_browser.addheaders = [('User-agent', user_agent_rotator.get_random_user_agent())]
+    my_browser.open(rng_url)
+
+    # Seleciona o formulário a ser preenchido
+    my_browser.select_form(nr = 1)
+
+    return my_browser
+
+
+def auto_generation(my_browser):
+
+    # Preenche os valores do intervalo nos campos
+    my_browser.form['numbersfrom'] = str(min)
+    my_browser.form['numbersto'] = str(max)
+
+    # Quantidade de números a serem gerados definida aleatoriamente a cada requisição
+    n = random.randint(800, 1000)
+    my_browser.form['number'] = str(n)
+    
+    # Executa a requisição e recebe o arquivo HTML referente à resposta da mesma
+    my_browser.submit()
+    response = my_browser.response().read()
 
     numbers = list()
-    soup = BeautifulSoup(raw_html, 'lxml')
+    soup = BeautifulSoup(response, 'lxml')
     
+    # Obtém o texto contendo o(s) número(s) gerados na resposta da requisição (tag HTML <th>)
     r_text = soup.find('th').text
     str_numbers = r_text.split(', ')
 
+    # Converte todos os números da lista para inteiros (str -> int)
     numbers = list(map(int, str_numbers))
     return numbers
 
 
 def randomness_evaluation():
 
+    print('Inicializando...\n')
     all_numbers = list()
+    my_browser = initialize_browser()
 
-    for i in range(total):
+    # Enquanto não forem gerados números suficientes
+    while len(all_numbers) < total:
 
-        response = input_rng()
-        generated_numbers = rng_scrap(response)
+        # Preenche os campos da paǵina, obtém os números gerados e adiciona-os na lista
+        generated_numbers = auto_generation(my_browser)
         all_numbers += generated_numbers
 
-        del(response, generated_numbers)
+        del(generated_numbers)
+        progress = (len(all_numbers) / total) * 100.0
+        print(f'{round(progress, 2)} concluído...')
 
-        timing = random.randint(2, 5)
+        # Tempo definido aleatoriamente entre cada requisição para evitar bloqueio por IP
+        timing = random.randint(2, 4)
         sleep(timing)
 
-    print(len(all_numbers))
-    print(len(set(all_numbers)))
+    my_browser.close()
+
+    # Resultado
+    print('\nFinalizado!\n')
+    size = len(all_numbers)
+    uniques = len(set(all_numbers))
+    print(f'{size - uniques} repetições em {size} códigos gerados!')
         
 
 if __name__ == '__main__':
-    pass
-    # randomness_evaluation()
+    randomness_evaluation()
