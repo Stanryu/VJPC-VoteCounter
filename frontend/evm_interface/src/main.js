36b2c0b9fae4a7e9030672c1bfd7a14d419d6c88
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-04-09 01:03:43 -0300

    feat: Added FontAwesome icons to improve GUI

--- frontend/evm_interface/src/main.js
+++ frontend/evm_interface/src/main.js
@@ -6,6 +6,22 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import BootstrapVue from 'bootstrap-vue';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faCheckCircle, faExclamationCircle)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
